acfabe29e9f569229b8ddd47aadbcb5a2e5e280d
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-12-11 19:52:02 +0000

    #4 Created view for signing up, html pages for login and signup and also connected .html pages with views on urls.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import SignUpForm
 
 # Create your views here.
+class SignUp(CreateView):
+    form_class = SignUpForm
+    # Redirect to login page post successful SignUp
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
+
