2b6552602eee59b004792dacea3522d62d1afeae
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-12-18 19:26:47 +0000

    #12 Created view for creating a group, showing details of a group and listing all the groups on a page

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
+from models import Group
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    """ This class method creates group """
+    fields = ['name', 'description']
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    """ This class creates detail view for a single group """
+    model = Group
+
+class ListGroup(generic.ListView):
+    """ This class lists all the created groups """
+    model = Group
