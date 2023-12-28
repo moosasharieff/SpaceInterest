67281924433361c4b8ac09428124f079579fd58a
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-12-28 15:25:49 +0000

    #13 Adding view for viewing all the posts in the form of list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+from django.http import Http404
+from django.views import generic
+
+from braces.views import SelectRelatedMixin
+
+from . import models
+from . import forms
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class PostList(SelectRelatedMixin, generic.ListView):
+    """
+    Connecting the model for Posts and adding relationship between 'user' and 'group'
+    """
+    model = models.Post
+    select_related = ('user', 'group')
+
