32fab8d67eb4cd39d7c02ec11cc7f29ab18e2048
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-12-28 15:38:15 +0000

    #13 Adding view for viewing all the posts of a specific user by querrying in the database and displaying it as a list using Class Based ListView.

--- posts/views.py
+++ posts/views.py
@@ -22,3 +22,26 @@ class PostList(SelectRelatedMixin, generic.ListView):
     model = models.Post
     select_related = ('user', 'group')
 
+class UserPosts(generic.ListView):
+    """
+    Viewing all the posts of a specific user is list form
+    """
+    model = models.Post
+    template_name = "post/user_post_list.html"
+
+    def get_queryset(self):
+        """
+        Querying database for a specific user and fetching all the posts of his.
+        """
+        try:
+            # checking if user exists and fetching its posts
+            self.post.user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+
+     def get_context_data(self, *args, **kwargs):
+         context = super().get_context_data(**kwargs)
+         context['post_user'] = self.post_user
+         return context
